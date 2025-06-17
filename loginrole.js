ebe9df677d667484537b23cf1e12edc43c7217b8
mhhha-art <haidermariam717@gmail.com>
2025-06-17 08:34:38 +0400

    Update loginrole.js

--- loginrole.js
+++ loginrole.js
@@ -48,17 +48,3 @@ document.querySelectorAll('.install-btn').forEach(button => {
 });
 
 
-
-    // Service Worker registration (from index.html)
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("service-worker.js")
-          .then((reg) => {
-            console.log("Service Worker registered!", reg);
-          })
-          .catch((err) => {
-            console.log("Service Worker registration failed: ", err);
-          });
-      });
-    }
